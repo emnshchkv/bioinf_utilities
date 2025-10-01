47919cdc6dee59299c75c3dd93cb058482502228
Елизавета Меньщикова <elizavetamenshchikova@MacBook-Air-Elizaveta.local>
2025-10-01 13:26:58 +0300

    Modify reverse_complement.py

---
+++ scripts/reverse_complement.py
@@ -0,0 +1,20 @@
+import is_nucleic_acid
+import complement
+
+
+def reverse_complement(sequence: str) -> str:
+    """
+    Converts a sequence into its reverse-complement counterpart.
+
+    Arguments:
+    sequence: str
+
+    Returns:
+    str: sequence reverse-complemet counterpart
+    None: if sequence is NOT a nucleic acid
+    """
+    if is_nucleic_acid.is_nucleic_acid(sequence):
+        comp_seq = complement.complement(sequence)
+        return comp_seq[::-1]
+    else:
+        return None
