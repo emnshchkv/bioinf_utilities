40d22cc2140c5b1f2edb8799c50976a7b4fd6fac
Елизавета Меньщикова <elizavetamenshchikova@MacBook-Air-Elizaveta.local>
2025-10-01 13:09:32 +0300

    Modify reverse.py

---
+++ scripts/reverse.py
@@ -0,0 +1,16 @@
+import is_nucleic_acid
+
+
+def reverse(sequence: str) -> str:
+    """
+    Converts a sequence into its reverse counterpart.
+
+    Arguments:
+    sequence: str
+
+    Returns:
+    str: reversed sequence
+    None: if sequence is NOT a nucleic acid
+    """
+    if is_nucleic_acid(sequence):
+        return sequence[::-1]
