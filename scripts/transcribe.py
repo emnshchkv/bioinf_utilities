9de678f67cfa692e88da1ed28a08ebb34c312c6a
Елизавета Меньщикова <elizavetamenshchikova@MacBook-Air-Elizaveta.local>
2025-10-01 13:21:03 +0300

    Modify transcribe.py

---
+++ scripts/transcribe.py
@@ -0,0 +1,34 @@
+import is_nucleic_acid
+
+
+def transcribe(
+    sequence: str,
+    transcription_dict: dict = {
+        "A": "U",
+        "a": "u",
+        "T": "A",
+        "t": "a",
+        "G": "C",
+        "g": "c",
+        "C": "G",
+        "c": "g",
+    },
+) -> str:
+    """
+    Generates a trancsript for a DNA sequence.
+
+    Arguments:
+    sequence: str
+    transcription_dict: dict
+
+    Returns:
+    str: for a DNA sequence
+    None: for a RNA sequence
+    """
+    if is_nucleic_acid.is_nucleic_acid(sequence):
+        mrna = ""
+        for nucleotide in sequence:
+            mrna += transcription_dict[nucleotide]
+        return mrna
+    else:
+        return None
