613cc2ec84ed57cf1c74d6b772f8858f5fcbbfec
Елизавета Меньщикова <elizavetamenshchikova@MacBook-Air-Elizaveta.local>
2025-10-01 13:24:41 +0300

    Modify complement.py

---
+++ scripts/complement.py
@@ -0,0 +1,47 @@
+import which_nucleic_acid
+
+
+def complement(sequence: str) -> str:
+    """
+    Converts a sequence into its complement counterpart.
+
+    Arguments:
+    sequence: str
+
+    Returns:
+    str: sequence complemet counterpart
+    None: if sequence is NOT a nucleic acid
+
+    """
+    if which_nucleic_acid.which_nucleic_acid(sequence) == "DNA":
+        comp_dict = {
+            "A": "T",
+            "a": "t",
+            "T": "A",
+            "t": "a",
+            "G": "C",
+            "g": "c",
+            "C": "G",
+            "c": "g",
+        }
+        countepart = ""
+        for nucleotide in sequence:
+            countepart += comp_dict[nucleotide]
+        return countepart
+    elif which_nucleic_acid.which_nucleic_acid(sequence) == "RNA":
+        comp_dict = {
+            "A": "U",
+            "a": "u",
+            "U": "A",
+            "u": "a",
+            "G": "C",
+            "g": "c",
+            "C": "G",
+            "c": "g",
+        }
+        countepart = ""
+        for nucleotide in sequence:
+            countepart += comp_dict[nucleotide]
+        return countepart
+    else:
+        return None
