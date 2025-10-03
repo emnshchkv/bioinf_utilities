363947dd292404e945402cf5845e74996850000b
emnshchkv <menshchikovaeliza@gmail.com>
2025-10-03 15:23:40 +0300

    Unite FASTQ filtering tools functions in one module

---
+++ scripts/filter_fastq_tools.py
@@ -0,0 +1,31 @@
+def gc_score(sequence: str) -> float:
+    """
+    Calculates GC-content score of a sequence.
+
+    Arguments:
+    sequence: str
+
+    Returns:
+    float: GC-content
+    """
+    sequence = sequence.lower()
+    return (sequence.count("g") + sequence.count("c")) * 100 / len(sequence)
+
+
+def quality_score(quality_line: str) -> float:
+    """
+    Calculates quatily score for a pair (sequence, quality).
+
+    Arguments:
+    quality_line: str
+
+    Returns:
+    float: average sequence quality
+    """
+    encoding = dict()
+    for x in range(0, 41):
+        encoding.update({chr(x + 33): int(x)})
+    total_score = 0
+    for sym in quality_line:
+        total_score += encoding[sym]
+    return total_score / len(quality_line)
