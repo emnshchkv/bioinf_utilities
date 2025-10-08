6077ec7857a850f90916a37dc084496a235966fc
emnshchkv <menshchikovaeliza@gmail.com>
2025-10-08 12:36:00 +0300

    Modify filter_fastq

--- bioinf_utilities.py
+++ bioinf_utilities.py
@@ -41,33 +41,34 @@ def run_dna_rna_tools(*funnel: str) -> Union[str, list[str | None], bool]:
 
 
 def filter_fastq(
-    seqs: dict[str, tuple[str, str]],
+    *fastq_path: str,
     gc_bounds: int | tuple[int | float, int | float] = (0, 100),
     length_bounds: int | tuple[int, int] = (0, 2**32),
     quality_threshold: int = 0,
-) -> dict:
+) -> None:
     """
     Filtering fastq with manual settings.
 
     Agruments:
-    seqs: dict
+    *fastq_path: absolute path to *.fastq
     gc_bounds: int | tuple
     length_bounds: int | tuple
     quality_threshold: int
 
     Returns:
-    dict: filtered fastq
+    None
     """
-    filtered = dict()
+    filtered_sequences = dict()
     (gc_lower_bound, gc_upper_bound), (len_lower_bound, len_upper_bound) = (
         assistant_ulitities.make_interval(gc_bounds),
         assistant_ulitities.make_interval(length_bounds),
     )
+    seqs = filter_fastq_tools.read_fastq(*fastq_path)
     for name, data in seqs.items():
         if (
             (gc_lower_bound <= filter_fastq_tools.gc_score(data[0]) <= gc_upper_bound)
             and (quality_threshold <= filter_fastq_tools.quality_score(data[1]))
             and (len_lower_bound <= len(data[0]) <= len_upper_bound)
         ):
-            filtered[name] = data
-    return filtered
+            filtered_sequences[name] = data
+    filter_fastq_tools.write_fastq(filtered_sequences)
